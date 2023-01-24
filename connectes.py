69b9e1ea0a31a8ebea71be0cfc41ae445b5f4095
Niels Favre-Nicolin <favrenin@ensipc432.ensimag.fr>
2023-01-24 09:37:40 +0100

    completé fct adj

--- connectes.py
+++ connectes.py
@@ -8,7 +8,7 @@ sort and display.
 from timeit import timeit
 from sys import argv
 
-from geo.point import Point
+from geo.point import Point, distance_to
 
 
 def load_instance(filename: str):
@@ -23,10 +23,18 @@ def load_instance(filename: str):
 
     return distance, points
 
-def liste_adjascence(distance, points):
+def liste_adjascence(distance: float, points: list[Point]):
     """Renvoit un dictionnaire ayant pour entrée les points et affichant 
     la liste de leurs points adjascents
     """
+    adj_list = {}
+    for point in points:
+        adj_list[f"{point}"] = []
+        for autre_point in points:
+            if point.distance_to(autre_point) < distance:
+                adj_list[f"{point}"].append(autre_point)
+    return adj_list
+
 
 def print_components_sizes(distance: float, points: list[Point]):
     """
